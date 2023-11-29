f4d7cffef232c7b99ffd1c4d4888ca877426f250
Barry <barrmull@gmail.com>
2023-11-29 15:49:20 +0100

    Add home page layout mark up and styling

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,27 @@
 import "@styles/globals.css";
 
+import Nav from '@components/Nav';
+import Provider from "@components/Provider";
+
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
+  title: "JokeShare",
+  description: "Discover & Share jokes",
 };
 
 const RootLayout = ({ children }) => (
   <html lang='en'>
     <body>
+    <Provider>
         <div className='main'>
           <div className='gradient' />
         </div>
 
         <main className='app'>
+          <Nav />
           {children}
         </main>
+      </Provider>
+
     </body>
   </html>
 );
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import Feed from '@components/Feed'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full flex-center flex-col'>
+        <h1  className='head_text text-center'>
+            Connect and Share your jokes, no matter how bad 
+            <br className='max-md:hidden' />
+            <span className='orange_gradient text-center'>Cheese it Up!</span>
+            <p className='desc text-center'>
+                joke share is a platform for sharing jokes, especially cringeworthy dad jokes
+            </p>
+        </h1>
+    </section>
   )
 }
 
