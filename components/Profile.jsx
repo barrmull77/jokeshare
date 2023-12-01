b145a4e00fbbe212956cbbd3dbedc16bd56ca657
Barry <barrmull@gmail.com>
2023-12-01 15:07:01 +0100

    remove unnecessary console.log

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,7 +1,7 @@
 import JokeCard from "./JokeCard";
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
-    console.log('data----', data);
+
   return (
     <section className='w-full'>
       <h1 className='head_text text-left'>
